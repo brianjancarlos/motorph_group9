2edf595a7417c8cca985efeec25c31501c870582
Brian Jan Carlos <41847348+brianjancarlos@users.noreply.github.com>
2024-01-20 19:46:48 +0800

    First package commit

---
+++ MotorPHSystem/src/main/java/com/group9/motorphsystem/MotorPHSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.group9.motorphsystem;
+
+/**
+ *
+ * @author brianjancarlos
+ */
+public class MotorPHSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
